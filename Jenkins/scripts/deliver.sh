f55e1a402b76519b317724ca3063849fb2504b69
vivek-5 <vivek.sundaramoorthy@gmail.com>
2024-08-30 15:09:19 +0530

    folder

--- Jenkins/scripts/deliver.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-npm run ng build
-npm run ng serve &
-sleep 1
-echo $! > .pidfile
-
-echo 'Now...'
-echo 'Visit http://localhost:4200 to see your Node.js/Angular application in action.'
